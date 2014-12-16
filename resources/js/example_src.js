0e9464ff22586dee0c10f6ae44bcbba1a3d2ef80
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-12-16 17:57:57 -0500

    formie with multipart

--- resources/js/example_src.js
+++ resources/js/example_src.js
@@ -2,13 +2,36 @@
 
 var formie = require('../../index'),
 	formie1,
+	formie2,
 	responseContainer;
 
 window.addEventListener('load', function () {
 	responseContainer = document.querySelector('#formie-test-result');
 	formie1 = new formie({
+		ajaxsubmitselector: '#formie-test',
+		postdata: {
+			_csrf: document.querySelector('input[name="_csrf"]').value
+		},
 		queryparameters: {
-			format: 'json'
+			format: 'json',
+			_csrf: document.querySelector('input[name="_csrf"]').value
+		},
+		successcallback: function (response) {
+			responseContainer.innerHTML = JSON.stringify(response.body, null, 2);
+		}
+	});
+
+	formie2 = new formie({
+		ajaxsubmitselector: '#upload-formie-test',
+		postdata: {
+			_csrf: document.querySelector('input[name="_csrf"]').value
+		},
+		queryparameters: {
+			format: 'json',
+			_csrf: document.querySelector('input[name="_csrf"]').value
+		},
+		headers: {
+			_csrf: document.querySelector('input[name="_csrf"]').value
 		},
 		successcallback: function (response) {
 			responseContainer.innerHTML = JSON.stringify(response.body, null, 2);
